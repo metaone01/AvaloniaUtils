a6800bcd00935366ea97896b10ea3261bdffa77a
metaone01 <metaone01@outlook.com>
2025-06-21 21:42:27 +0800

    Update

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-﻿
+﻿# Avalonia Utils
+
+Provide some utilities for Avalonia.
+
+## Get Started
+
+### Add NuGet Package:
+
+```bash
+dotnet add package AvaloniaUtils
+```
+
+### Include in Your Axaml:
+```xaml
+xmlns:utils="https://github.com/metaone01/AvaloniaUtils"
+```
+
+## GridUtils
+
+Arguments in *[ ]* means they are optional.
+
+`Grid.Position`: `Row`\[+`RowSpan`\] `Column` \[+ `ColumnSpan`\]
+```xaml
+<CONTROL utils:Grid.Position="1+2 2"/>
+<!-- ↑ using AvaloniaUtils ↑ -->
+
+<!-- ↓ Avalonia default ↓ -->
+<CONTROL Grid.Row="1" Grid.Column="2" Grid.RowSpan="2"/>
+```
--- src/GridUtils.cs
+++ src/GridUtils.cs
@@ -7,25 +7,24 @@ namespace AvaloniaUtils;
 public readonly record struct GridPosition(int Row = 0, int Column = 0, int RowSpan = 1, int ColumnSpan = 1) {
     public static GridPosition Parse(string s) {
         string[] t = s.Split(' ');
-        if (t.Length is 0 or > 2)
+        if (t.Length != 2)
             throw new FormatException($"{s} is not a valid grid position.");
         // ReSharper disable once RedundantAssignment
         int row = 0, column = 0, rowSpan = 1, columnSpan = 1;
+        
         string[] rowPosition = t[0].Split('+');
         if (rowPosition.Length is 0 or > 2)
             throw new FormatException($"{rowPosition} is not a valid row position.");
         row = int.Parse(rowPosition[0]);
         if (rowPosition.Length == 2)
             rowSpan = int.Parse(rowPosition[1]);
-        // ReSharper disable once InvertIf
-        if (t.Length == 2) {
-            string[] columnPosition = t[1].Split('+'); 
-            if (columnPosition.Length is 0 or > 2)
-                throw new FormatException($"{columnPosition} is not a valid column position.");
-            column = int.Parse(columnPosition[0]);
-            if (columnPosition.Length == 2)
-                columnSpan = int.Parse(columnPosition[1]);
-        }
+        
+        string[] columnPosition = t[1].Split('+');
+        if (columnPosition.Length is 0 or > 2)
+            throw new FormatException($"{columnPosition} is not a valid column position.");
+        column = int.Parse(columnPosition[0]);
+        if (columnPosition.Length == 2)
+            columnSpan = int.Parse(columnPosition[1]);
 
         return new GridPosition(row, column, rowSpan, columnSpan);
     }
@@ -44,10 +43,10 @@ public class Grid : AvaloniaObject {
         PositionProperty.Changed.AddClassHandler<Control>((control, args) => {
             if (args.NewValue is not GridPosition newPosition)
                 return;
-            Avalonia.Controls.Grid.SetRow(control,newPosition.Row);
-            Avalonia.Controls.Grid.SetColumn(control,newPosition.Column);
-            Avalonia.Controls.Grid.SetRowSpan(control,newPosition.RowSpan);
-            Avalonia.Controls.Grid.SetColumnSpan(control,newPosition.ColumnSpan);
+            Avalonia.Controls.Grid.SetRow(control, newPosition.Row);
+            Avalonia.Controls.Grid.SetColumn(control, newPosition.Column);
+            Avalonia.Controls.Grid.SetRowSpan(control, newPosition.RowSpan);
+            Avalonia.Controls.Grid.SetColumnSpan(control, newPosition.ColumnSpan);
         });
     }
 }
